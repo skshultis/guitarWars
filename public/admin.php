39cbb159660cf533013ce996001f82855aaff495
skshultis <shultis.sk@gmail.com>
2017-06-01 15:02:51 -0400

    admin page up

---
+++ public/admin.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once('appvars.php');
+require_once('connectvars.php');
+
+// db connection
+$dbc = mysqli_connect('127.0.0.1', 'homestead', 'secret', 'guitar');
+
+// gets scores from db
+$query = "SELECT * FROM guitarwars ORDERBY score DESC, date ASC";
+$data = mysqli_query($dbc, $query);
+
+// loop through score data, formatting as html table
+echo '<table>';
+
+while ($row = mysqli_fetch_array($data))  {
+
+    echo '<tr class="scorerow"><td><strong>' . $row['name'] . '</strong></td>';
+    echo '<td>' . $row['date'] . '</td>';
+    echo '<td>' . $row['score'] . '</td>';
+    echo '<td><a href="removescore.php?id=' . $row['id'] . '&amp;date=' . $row['date'] .
+      '&amp;name=' . $row['name'] . '&amp;score=' . $row['score'] .
+      '&amp;screenshot=' . $row['screenshot'] . '">Remove</a></td></tr>';
+
+}
+
+echo '</table>';
+
+mysqli_close($dbc);
+
+ ?>
--- public/removescore.php
+++ public/removescore.php
@@ -1,4 +1,5 @@
 <?php
+
   require_once('appvars.php');
 
   $dbc = mysqli_connect('127.0.0.1', 'homestead', 'secret', 'guitar');
@@ -6,13 +7,23 @@
   $query = "SELECT * FROM guitarwars ORDER BY score DESC, date ASC";
   $data = mysqli_query($dbc, $query);
 
+  echo '<h2>Welcome to guitarNerds Admin</h2>';
+
+  echo '<hr />';
+
   echo '<table>';
+
   while ($row = mysqli_fetch_array($data))  {
+
     echo '<tr class="scorerow"><td><strong>' . $row['name'] . '</strong></td>';
     echo '<td> ' . $row['date'] . '</td>';
     echo '<td>' . $row['score'] . '</td>';
     echo '<td><a href="removescore.php?id=' . $row['id'] . '$amp;date=' . $row['date'] . '$amp;=name' . $row['name'] . '$amp;score=' . $row['score'] . '$amp;screenshot=' .  $row['screenshot'] . '">Remove</a></td></tr>';
+
   }
+
   echo '</table>';
+
   mysqli_close($dbc);
+
  ?>
